9df1f7a0416c667c5778684171d592e6c432257e
Kenta Wakasa <wakasa.kenta.pg@gmail.com>
2022-07-29 18:59:58 +0900

    add unit test for formatMinimum

--- test/format_xxx/format_xxx_test.dart
+++ test/format_xxx/format_xxx_test.dart
@@ -3,6 +3,25 @@ import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
+  test('formatMinimum test', () {
+    final schema = {
+      'type': 'string',
+      'format': 'date-time',
+      'formatMinimum': '2022-07-02T00:00:00', // 7/2以降
+    };
+
+    final jsonSchema = JsonSchema.createSchema(
+      schema,
+      schemaVersion: SchemaVersion.draft6,
+    );
+
+    final normalValue = '2022-07-02T00:00:00';
+    final abnormalValue = '2022-07-01T00:00:00';
+
+    expect(jsonSchema.validate(normalValue), true);
+    expect(jsonSchema.validate(abnormalValue), false);
+  });
+
   test('formatXXX test', () {
     final schema = {
       'type': 'string',
